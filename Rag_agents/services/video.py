64e61a391ac04538fa0f27745feb392046bd9a81
swati <swatidutta993@gmial.com>
2025-05-15 13:21:25 +0545

    Implement logger.

--- Rag_agents/services/video.py
+++ Rag_agents/services/video.py
@@ -8,21 +8,21 @@ logger = logging.getLogger(__name__)
 
 llm_model = ChatOpenAI(model="gpt-4o-mini")
 
+
 class VideoNode(BaseNode):
     def run(self, state: OverallState) -> OverallState:
         prompt = f"""Using the following script, create a short visual plan or concept for a video:
 
 Script:
-'{state['script']}'
+'{state["script"]}'
 
 Output should describe the video format, pacing, shots, and any visual ideas. Include hashtag ideas if relevant."""
-        
+
         response = llm_model.invoke(prompt)
         print(f"[Video Node]\n{response.content}")
 
         # Update state dictionary directly
-        state['video'] = response.content
+        state["video"] = response.content
         logger.info("Successfully invoked video generation function.")
-
 
         return state  # Return the updated state
