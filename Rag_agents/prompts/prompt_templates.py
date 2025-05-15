f92820d767e32e5c65771b615e6ea14afe2af56d
swati <swatidutta993@gmial.com>
2025-05-15 13:23:36 +0545

    Improve system prompt.

--- Rag_agents/prompts/prompt_templates.py
+++ Rag_agents/prompts/prompt_templates.py
@@ -1,9 +1,12 @@
 from langchain.prompts import PromptTemplate
 
+
 class SystemPrompts:
     prompt_template = PromptTemplate.from_template(
-    """You are a video content assistant.
+        """You are a video content assistant.
 Given a brand-related topic: {topic} and trending hashtags,
 suggest a video theme or idea.
-Mention the style, tone, and key points in 2-3 lines."""
-)
+If the user enters the prompt beside the tools it can invoke, the system should kindly return the output saying we don't have 
+information about it in our knowledge base.
+"""
+    )
