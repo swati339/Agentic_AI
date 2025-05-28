ebdf536ee0b101906866b0a20e921fbf4cb642a3
swati <swatidutta993@gmial.com>
2025-05-28 08:35:09 +0545

    Detail prompting.

--- Rag_agents/services/video.py
+++ Rag_agents/services/video.py
@@ -16,7 +16,7 @@ class VideoNode(BaseNode):
 Script:
 '{state["script"]}'
 
-Output should describe the video format, pacing, shots, and any visual ideas. Donot include hashtags."""
+Output should describe the video format, pacing, shots, and any visual ideas."""
 
         response = llm_model.invoke(prompt)
         print(f"[Video Node]\n{response.content}")
