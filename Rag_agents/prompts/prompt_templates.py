5d1751b960c5314b9cd3199592de18a5ca8d32df
swati <swatidutta993@gmial.com>
2025-05-28 08:37:27 +0545

    New promot.

--- Rag_agents/prompts/prompt_templates.py
+++ Rag_agents/prompts/prompt_templates.py
@@ -12,8 +12,10 @@ Given:
 Your task is to generate content **based on the user's intent**. The user may request:
 - Only a **script** for the video
 - A list of **relevant hashtags**
-- A complete **video plan** including script, shoot locations, and hashtags
+- A complete **video plan** including script, shoot locations.
 - Or any **combination** of the above.
+-Please generate the output based on the user prompt.
+-Donot answer if the user prompt doesnot match the tool calls.
 
  Follow these rules:
 - Generate only the part that is required for the current step: "{current_step}"
